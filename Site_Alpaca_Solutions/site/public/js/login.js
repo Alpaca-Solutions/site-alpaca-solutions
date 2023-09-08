dc354b163b430e010105b624f3fd54782d049165
EwertonLima2002 <ewerton.lima860@sptech.school>
2023-09-08 20:36:37 -0300

    Documentando o código e pequenos detalhes visuais de validação

--- Site_Alpaca_Solutions/site/public/js/login.js
+++ Site_Alpaca_Solutions/site/public/js/login.js
@@ -1,15 +1,34 @@
 function validar_email(){
     let input_email = document.getElementById("input_email");
     let label_email = document.getElementById("email_label");
-    var email = input_email.value
+    var email = input_email.value.toLowerCase();
 
-    if(email.length < 7 || !email.endsWith("@admin.com") && !email.endsWith("@gmail.com")){
+    // Guys aqui ele converte para minusculo para validar na linha 21
+    email = email.toLowerCase();
+
+
+    // aqui verifica se ta vazio
+    if(email == ""){
+        input_email.classList.add("erro_input");
+        label_email.innerHTML = "Campo vazio";
+        label_email.style.color = "red";
+        input_email.classList.remove("input_ok");   
+    }
+
+    // aqui aplicamos a regra de negocio que tem que ter o final @admin.com ou então é um chefe por exemplo
+    // que usar um email normal
+  
+    else if(email.length < 7 || !email.endsWith("@admin.com") && !email.endsWith("@gmail.com")){
        input_email.classList.add("erro_input");
-       label_email.style.color = "red"
+       label_email.innerHTML = "Email inválido";
+       label_email.style.color = "red";
+       input_email.classList.remove("input_ok");
     }
     else{
         input_email.classList.remove("erro_input");
-        label_email.style.color = "#ffff"
+        label_email.innerHTML = "Email";
+        input_email.classList.add("input_ok");
+        label_email.style.color = "#ffff";
     }
 
 }
@@ -17,22 +36,35 @@ function validar_email(){
 function validar_senha(){
     let input_senha = document.getElementById("input_senha");
     let label_senha = document.getElementById("senha_label");
-    var senha = input_senha.value
+    var senha = input_senha.value;
 
-    if(senha.length < 8){
+
+    if(senha == ""){
         input_senha.classList.add("erro_input");
         input_senha.classList.remove("senha_media");
-        label_senha.style.color = "red"
+        label_senha.style.color = "red";
+        input_senha.classList.remove("input_ok");
+        label_senha.innerHTML = "Campo vazio";
+    }
+   else if(senha.length < 8){
+        input_senha.classList.add("erro_input");
+        input_senha.classList.remove("senha_media");
+        label_senha.style.color = "red";
+        label_senha.innerHTML = "Senha inválida";
     }
     else if(senha.length >=8 && senha.length < 10){
         input_senha.classList.add("senha_media");
         input_senha.classList.remove("erro_input");
-        label_senha.style.color = "rgb(255, 151, 15)"
+        input_senha.classList.remove("input_ok");
+        label_senha.innerHTML = "Senha nota 7";
+        label_senha.style.color = "rgb(255, 151, 15)";
     }
     else{
         input_senha.classList.remove("erro_input");
         input_senha.classList.remove("senha_media");
-        label_senha.style.color = "#ffff"
+        input_senha.classList.add("input_ok");
+        label_senha.innerHTML = "Senha";
+        label_senha.style.color = "#FFFF"
     }
 
 
