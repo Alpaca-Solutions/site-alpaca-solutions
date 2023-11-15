786b415afd4ac74a356246ad3008de1f9649ec17
annamatos <anna.matos@sptech.school>
2023-11-14 21:34:00 -0300

    Inserindo cadastros de empresa, usuario e maquina no BD

--- Site_Alpaca_Solutions/site/src/models/usuarioModel.js
+++ Site_Alpaca_Solutions/site/src/models/usuarioModel.js
@@ -3,20 +3,24 @@ var database = require("../database/config")
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucao = `
-        SELECT id, nome, email, fk_empresa as empresaId FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+    SELECT * FROM Endereco
+    JOIN Empresa ON Empresa.fk_endereco = Endereco.idEndereco
+    JOIN Telefone ON Telefone.fk_cliente = Empresa.idEmpresa
+    JOIN Usuario ON Usuario.fk_cliente_usuario = Empresa.idEmpresa WHERE email = '${email}' AND senha = '${senha}';;
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucao
-function cadastrar(nome, email, senha, empresaId) {
+function cadastrarUsuario(nome, email, senha, tipoAcesso, nivelAcesso, empresaId) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucao = `
-        INSERT INTO usuario (nome, email, senha, fk_empresa) VALUES ('${nome}', '${email}', '${senha}', '${empresaId}');
+    INSERT INTO Usuario (email, senha, tipo_acesso, nivel_acesso, fk_cliente_usuario)
+    VALUES ('${email}', '${senha}', '${tipoAcesso}', '${nivelAcesso}', '1');
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
@@ -24,5 +28,5 @@ function cadastrar(nome, email, senha, empresaId) {
 
 module.exports = {
     autenticar,
-    cadastrar
+    cadastrarUsuario
 };
