25358de3679b4fd18adae9016f951fb9ad2bca41
EwertonLima2002 <ewerton.lima860@sptech.school>
2023-12-09 17:50:46 -0300

    Novo teste

--- Site_Alpaca_Solutions/site/src/models/usuarioModel.js
+++ Site_Alpaca_Solutions/site/src/models/usuarioModel.js
@@ -71,17 +71,15 @@ WHERE Usuario.email = '${email}' AND Usuario.senha = '${senha}' AND Usuario.ativ
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucao
 function cadastrarUsuario(nome, email, senha, tipoAcesso, nivelAcesso, empresaId) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n", nome, email, senha);
 
-    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-    //  e na ordem de inserção dos dados.
+
     if (process.env.AMBIENTE_PROCESSO == "producao") {
-    var instrucao = `
-    INSERT INTO Usuario (nome, email, senha, tipoAcesso, nivelAcesso, ativo, fkEmpresa)
-    VALUES ('${nome}','${email}', '${senha}', '${tipoAcesso}', '${nivelAcesso}', 1, ${empresaId});
-    `;
-    }
-    else if(process.env.AMBIENTE_PROCESSO == "desenvolvimento"){
+        var instrucao = `
+        INSERT INTO Usuario (nome, email, senha, tipoAcesso, nivelAcesso, ativo, fkEmpresa)
+        VALUES ('${nome}','${email}', '${senha}', '${tipoAcesso}', '${nivelAcesso}', 1, ${empresaId});
+        `;
+    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         var instrucao = `
         INSERT INTO Usuario (nome, email, senha, tipoAcesso, nivelAcesso, ativo, fkEmpresa)
         VALUES ('${nome}','${email}', '${senha}', '${tipoAcesso}', '${nivelAcesso}', true, ${empresaId});
